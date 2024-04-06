d897d292896316ed973f3f3086e2b872a8814dd0
luisvill02 <lvillamil@unimagdalena.edu.co>
2024-04-05 23:45:05 -0500

    init test container and integration test

---
+++ src/test/java/com/ventas/tienda/TiendaApplicationTest.java
@@ -0,0 +1,11 @@
+package com.ventas.tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TiendaApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
--- src/test/java/com/ventas/tienda/repository/ShippingDetailsRepositoryTest.java
+++ src/test/java/com/ventas/tienda/repository/ShippingDetailsRepositoryTest.java
@@ -1,12 +1,48 @@
 package com.ventas.tienda.repository;
 
+import com.ventas.tienda.IntegrationDbRepositoryTest;
+import com.ventas.tienda.model.entities.Order;
+import com.ventas.tienda.model.entities.ShippingDetails;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-class ShippingDetailsRepositoryTest {
+class ShippingDetailsRepositoryTest extends IntegrationDbRepositoryTest {
+
+    private final ShippingDetailsRepository shippingDetailsRepository;
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    ShippingDetailsRepositoryTest(ShippingDetailsRepository shippingDetailsRepository, OrderRepository orderRepository) {
+        this.shippingDetailsRepository = shippingDetailsRepository;
+        this.orderRepository = orderRepository;
+    }
 
     @BeforeEach
     void setUp() {
+        shippingDetailsRepository.deleteAll();
+    }
+
+    void mockData() {
+        Order order = Order.builder()
+                .idOrder(1L)
+                .build();
+        ShippingDetails shippingDetails =  ShippingDetails.builder()
+                .carrier("Carrier")
+                .guideNumber(123456L)
+                .address("Address")
+                .order(order)
+                .build();
+        orderRepository.save(order);
+        shippingDetailsRepository.save(shippingDetails);
+    }
+
+    @Test
+    void givenShippingDetails_whenSave_thenDetailsId() {
+        mockData();
+        ShippingDetails shippingDetails = shippingDetailsRepository.findAll().get(0);
+        assertNotNull(shippingDetails.getIdDetail());
     }
 }
